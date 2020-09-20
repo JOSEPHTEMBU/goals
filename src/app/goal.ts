890be625d780393bfdb8b5257e684024a7e325cc
JOSEPHTEMBU <josephmulama30@gmail.com>
2020-09-20 15:38:13 +0300

    All work

---
+++ src/app/goal.ts
@@ -0,0 +1,15 @@
+export class Goal {
+  id: number;
+  name: string;
+  description: string;
+  completeDate: Date;
+  showDescription: boolean;
+
+  constructor(id: number, name: string, description: string, completeDate: Date) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.completeDate=  completeDate;
+    this.showDescription = false;
+  }
+}
